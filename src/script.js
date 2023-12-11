1f739fbb8bad30a9bc47da09eb00423195dad642
Ameer <ameerapex496@gmail.com>
2023-12-11 06:53:54 +0100

    added comment / added for each click

--- src/script.js
+++ src/script.js
@@ -1,1 +1,10 @@
-console.log("hallo world");
+// selecting our elements
+var numberInputs = document.querySelectorAll(".number-Input");
+// global veribales
+// functions
+// event linsters
+numberInputs.forEach(function (input) {
+    input.addEventListener("input", function (e) {
+        console.log(e.target.value);
+    });
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,13 @@
-console.log("hallo world");
+// selecting our elements
+const numberInputs = document.querySelectorAll(".number-Input");
+
+// global veribales
+
+// functions
+
+// event linsters
+numberInputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    console.log(e.target.value);
+  });
+});
