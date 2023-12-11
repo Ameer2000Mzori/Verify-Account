9bef82a0bb708522a91c5f035e68fca013574bca
Ameer <ameerapex496@gmail.com>
2023-12-11 07:11:44 +0100

    added nextEl function for cleaner code

--- src/script.js
+++ src/script.js
@@ -3,19 +3,22 @@ var numberInputs = document.querySelectorAll(".number-Input");
 // global veribales
 var indexNum = 0;
 // functions
+var nextEl = function (numbersVal) {
+    indexNum += 1;
+    if (numbersVal) {
+        if (indexNum >= 5) {
+            console.log("no boxes left");
+        }
+        else {
+            numberInputs[indexNum].focus();
+            console.log(indexNum);
+        }
+    }
+};
 // event linsters
 numberInputs.forEach(function (input) {
     input.addEventListener("input", function (e) {
         var numbersVal = e.target.value;
-        indexNum += 1;
-        if (numbersVal) {
-            if (indexNum >= 5) {
-                console.log("no boxes left");
-            }
-            else {
-                numberInputs[indexNum].focus();
-                console.log(indexNum);
-            }
-        }
+        nextEl(numbersVal);
     });
 });
--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,26 @@
 // selecting our elements
-const numberInputs = document.querySelectorAll(".number-Input");
+const numberInputs: any = document.querySelectorAll(".number-Input");
 
 // global veribales
 let indexNum = 0;
+
 // functions
+const nextEl = (numbersVal) => {
+  indexNum += 1;
+  if (numbersVal) {
+    if (indexNum >= 5) {
+      console.log("no boxes left");
+    } else {
+      numberInputs[indexNum].focus();
+      console.log(indexNum);
+    }
+  }
+};
 
 // event linsters
 numberInputs.forEach((input) => {
   input.addEventListener("input", (e: any) => {
     let numbersVal = e.target.value;
-    indexNum += 1;
-    if (numbersVal) {
-      if (indexNum >= 5) {
-        console.log("no boxes left");
-      } else {
-        numberInputs[indexNum].focus();
-        console.log(indexNum);
-      }
-    }
+    nextEl(numbersVal);
   });
 });
