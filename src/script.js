bd63e0b29f2492375a4c57329feed8e6d77323ad
Ameer <ameerapex496@gmail.com>
2023-12-11 07:10:26 +0100

    added , when input is 1 then focus th next element

--- src/script.js
+++ src/script.js
@@ -1,10 +1,21 @@
 // selecting our elements
 var numberInputs = document.querySelectorAll(".number-Input");
 // global veribales
+var indexNum = 0;
 // functions
 // event linsters
 numberInputs.forEach(function (input) {
     input.addEventListener("input", function (e) {
-        console.log(e.target.value);
+        var numbersVal = e.target.value;
+        indexNum += 1;
+        if (numbersVal) {
+            if (indexNum >= 5) {
+                console.log("no boxes left");
+            }
+            else {
+                numberInputs[indexNum].focus();
+                console.log(indexNum);
+            }
+        }
     });
 });
--- src/script.ts
+++ src/script.ts
@@ -2,12 +2,21 @@
 const numberInputs = document.querySelectorAll(".number-Input");
 
 // global veribales
-
+let indexNum = 0;
 // functions
 
 // event linsters
 numberInputs.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    console.log(e.target.value);
+  input.addEventListener("input", (e: any) => {
+    let numbersVal = e.target.value;
+    indexNum += 1;
+    if (numbersVal) {
+      if (indexNum >= 5) {
+        console.log("no boxes left");
+      } else {
+        numberInputs[indexNum].focus();
+        console.log(indexNum);
+      }
+    }
   });
 });
